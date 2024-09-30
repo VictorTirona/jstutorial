bc464d5e0a201368e98a34080385ed7b8a45d388
Victor Tirona <vlbtirona@gmail.com>
2024-09-30 18:24:01 +0800

    This is my first commit for the JavaScript Refresher

---
+++ myScript.js
@@ -0,0 +1,35 @@
+let x, y, z;
+x = 3;
+y = 10;
+z = 20;
+
+function showDate(){
+    document.getElementById('demo').innerHTML = Date()
+}
+function changeText(){
+    document.getElementById("demo").innerHTML = "Hello JavaScript!"
+}
+function changeSize(){
+    document.getElementById('demo').style.fontSize='35px'
+}
+function hideText(){
+    document.getElementById('demo').style.display='none'
+}
+function showText(){
+    document.getElementById('demo').style.display='block'
+}
+function beMegan(){
+    document.getElementById('demo2').innerHTML = "okane kasegu orera was star"
+}
+
+function alertMe(){
+    window.alert(x)
+}
+
+function logMe(){
+    console.log(y)
+}
+
+function printMe(){
+    window.print()
+}
